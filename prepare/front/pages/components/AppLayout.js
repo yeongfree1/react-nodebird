3e1be3456bc3c86a220e752ba06d0d659a46641f
한영관 <young@MacBook-Pro.local>
2020-12-15 23:58:17 +0900

    UPDATE: link and eslintrc update

--- prepare/front/pages/components/AppLayout.js
+++ prepare/front/pages/components/AppLayout.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import Link from 'next/link';
 
 const AppLayout= ({ children }) =>{
     return (
         <div>
-            <div>공통메뉴</div>
+            <div>
+                <Link href="/">노드버드</Link>
+                <Link href="/profile">프로필</Link>
+                <Link href="/signup">회원가입</Link>
+            </div>
             {children}
         </div>
     )
