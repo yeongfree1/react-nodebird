62bc50c1562fe52eff825b3d08abd7bf090f5f11
한영관 <young@MacBook-Pro.local>
2020-12-17 23:06:21 +0900

    eslint and prettier setting

--- prepare/front/pages/components/AppLayout.js
+++ prepare/front/pages/components/AppLayout.js
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-const AppLayout= ({ children }) =>{
-    return (
-        <div>
-            <div>
-                <Link href="/">노드버드</Link>
-                <Link href="/profile">프로필</Link>
-                <Link href="/signup">회원가입</Link>
-            </div>
-            {children}
-        </div>
-    )
-}
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <div>
+        <Link href="/">노드버드</Link>
+        <Link href="/profile">프로필</Link>
+        <Link href="/signup">회원가입</Link>
+      </div>
+      {children}
+    </div>
+  );
+};
 
 AppLayout.propTypes = {
-    children: PropTypes.node.isRequired
-}
+  children: PropTypes.node.isRequired,
+};
 
 export default AppLayout;
