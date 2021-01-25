ab5115965bf722b699055cd2eefbd32cd2b2c4dc
yeongfree1 <yeongfree1@gmail.com>
2021-01-25 14:52:20 +0900

    react-slick and imageZoom update

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,18 +1,23 @@
 import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
+import ImagesZoom from './imagesZoom';
 
 const PostImages = ({ images }) => {
-  const [showImages, setShowImagesZoom] = useState(false);
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
 
   const onZoom = useCallback(() => {
     setShowImagesZoom(true);
+  }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
   }, []);
 
   if (images.length === 1) {
     return (
       <>
         <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        {showImagesZoom && <ImagesZoom image={images} onClose={onClose} />}
       </>
     );
   }
@@ -34,6 +39,7 @@ const PostImages = ({ images }) => {
           alt={images[1].src}
           onClick={onZoom}
         />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
   }
@@ -62,6 +68,7 @@ const PostImages = ({ images }) => {
           {images.length - 1} 개의 사진 더보기
         </div>
       </div>
+      {showImagesZoom && <ImagesZoom image={images} onClose={onClose} />}
     </>
   );
 };
